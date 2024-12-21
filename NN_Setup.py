204f224405e181de0f5181608b54e2087fc0925f
NoeRicklin <noe.ricklin@outlook.de>
2024-12-21 14:15:19 +0100

    fixed NN_Setup.py

--- NN_Setup.py
+++ NN_Setup.py
@@ -1,27 +1,28 @@
 from random import random
+import os
 
 n_input = 42
 n_hlayers = [3,]
 n_output = 7
 
-NN_file = open("C:/Users/noeri/Documents/Schule/AngewandteMathematik/RegressionUndNeuronaleNetzwerke/VierGewinnt/Bot_Parameters.txt", "w")
+NN_file = open(os.path.dirname(__file__) + '\\Bot_Parameters.txt', "w")
 NN_file.close()
 
-NN_file = open("C:/Users/noeri/Documents/Schule/AngewandteMathematik/RegressionUndNeuronaleNetzwerke/VierGewinnt/Bot_Parameters.txt", "a")
+NN_file = open(os.path.dirname(__file__) + '\\Bot_Parameters.txt', "a")
 
 for hlayer in n_hlayers:
     layer_parameters = ""
-    for _ in range(hlayer):
-        layer_parameters += str(random()) + ","
-    layer_parameters = layer_parameters.rstrip(",") + "|"
-
-    for _ in range(hlayer):
-        layer_parameters += str(random()) + ","
-    layer_parameters = layer_parameters.rstrip(",")
+    for node in range(hlayer):
+        for input in range(n_input):
+            layer_parameters += str(random()) + ","
+        layer_parameters = layer_parameters.rstrip(",") + "|"
+        for input in range(n_input):
+            layer_parameters += str(random()) + ","
+        layer_parameters = layer_parameters.rstrip(",") + " "
     NN_file.write(layer_parameters)
 
 
 NN_file.close()
-NN_file = open("C:/Users/noeri/Documents/Schule/AngewandteMathematik/RegressionUndNeuronaleNetzwerke/VierGewinnt/Bot_Parameters.txt", "r")
+NN_file = open(os.path.dirname(__file__) + '\\Bot_Parameters.txt', "r")
 print(NN_file.read())
 NN_file.close()
