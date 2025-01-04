8d68f494f7c329215c6f55a3f8dba80b139828f8
NoeRicklin <noe.ricklin@outlook.de>
2025-01-04 12:58:32 +0100

    Minor Changes

--- Data_converter.py
+++ Data_converter.py
@@ -1,21 +1,26 @@
-import os
+game_data = open(
+    r"Documents\Schule\AngewandteMathematik\RegressionUndNeuronaleNetzwerke\VierGewinnt\connect_4\connect-4.data")
+single_game = game_data.read().split("\n")[1889].split(",")[:-1]
 
-game_data = open(os.path.dirname(__file__) + f'\\connect_4\\connect-4.data')
-single_games = game_data.read().split("\n")
+conv_game = [[0 for _ in range(6)] for _ in range(7)]
+for index, element in enumerate(single_game):
+    y = index % 6
+    x = index // 7
 
-def new_gameState(game_index):
-    raw_game = single_games[game_index].split(",")[:-1]
-    conv_game = [[0 for _ in range(6)] for _ in range(7)]
-    for index, element in enumerate(raw_game):
-        y = index % 6
-        x = index // 6
+    if element == "x":
+        value = 1
+    elif element == "o":
+        value = -1
+    else:
+        value = 0
 
-        if element == "x":
-            value = 1
-        elif element == "o":
-            value = -1
-        else:
-            value = 0
+    conv_game[x][y] = value
 
-        conv_game[x][y] = value
-    return conv_game
+assignments = {1: "x",
+               -1: "o",
+               0: " "}
+
+for y in range(6):
+    line = ""
+    for x in range(7):
+        line += assignments[conv_game[x][y]]
